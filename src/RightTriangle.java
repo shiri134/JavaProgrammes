94642364cfee59bb464e4b6caa709e3ef2b49562
twpathashala51 <twpathashala51@gmail.com>
2016-07-22 08:43:33 +0530

    Added RightTriangle  Method

---
+++ src/RightTriangle.java
@@ -0,0 +1,17 @@
+
+public class RightTriangle {
+    public static void main(String[] args){
+        int n=3;
+        int i=1;
+        while(i<=n)
+        {
+            for(int j=0;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+            i++;
+
+        }
+    }
+}
