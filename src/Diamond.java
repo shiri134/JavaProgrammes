3833b46ab4a50822b0d0977b6c9fcff46676f036
twpathashala51 <twpathashala51@gmail.com>
2016-07-22 11:07:59 +0530

    Added Diamond  Method

---
+++ src/Diamond.java
@@ -0,0 +1,42 @@
+
+public class Diamond {
+    public static void main(String[] args)
+    {
+        int n=3;
+        int space=n-1;
+        int numStar=1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=space;j>0;j--)
+            {
+                System.out.print(" ");
+            }
+            for(int k=numStar;k>0;k--)
+            {
+                System.out.print("*");
+            }
+
+            space=space-1;
+            numStar=numStar+2;
+            System.out.print("\n");
+
+        }
+        space=space+2;
+        numStar=numStar-4;
+        for(int i=1;i<n;i++)
+        {
+            for (int j=space;j>0 ;j-- ) {
+                System.out.print(" ");
+
+            }
+            for(int k=numStar;k>0;k--)
+            {
+                System.out.print("*");
+            }
+            space=space+1;
+            numStar=numStar-2;
+            System.out.print("\n");
+        }
+
+    }
+}
