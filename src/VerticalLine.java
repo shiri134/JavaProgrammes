a9644336055db5b2bc24c40f24588483a1611ee3
twpathashala51 <twpathashala51@gmail.com>
2016-07-22 08:36:29 +0530

    Added VerticalLine  Method

---
+++ src/VerticalLine.java
@@ -0,0 +1,15 @@
+
+public class VerticalLine {
+
+    public static void main(String[] args)
+    {
+        int n=3;
+        for(int i=0;i<n;i++)
+        {
+
+            System.out.println("*");
+
+        }
+    }
+
+}
