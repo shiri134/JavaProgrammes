4516a66b5066174a58c3541415d09aa5cc9dc262
twpathashala51 <twpathashala51@gmail.com>
2016-07-22 08:29:59 +0530

    Added LineOfStar Method

---
+++ src/LineOfStar.java
@@ -0,0 +1,11 @@
+public class LineOfStar {
+
+    public static void main(String[] args)
+    {
+        int n=8;
+        for(int i=0;i<n;i++){
+            System.out.print("*");
+        }
+
+    }
+}
